e4ac8cc33885a8c4ddc43e93dbb659bc1fb7a97c
busra <busratur1@gmail.com>
2021-12-30 09:11:31 +0300

    30.12.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind2';
+  title:string = 'northwind2';
+  user:string ="Büşra Birgül Tur";
+  
+  
 }
+//datayı yönettiğimiz yer .ts(type script)
